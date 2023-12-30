761265f0a712a5ae748c5beb68729bee25f2ae80
xatrarana <chhatraclas@gmail.com>
2023-12-30 15:22:08 +0545

    server-action,infinitescroll,framermotion

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MotionDiv } from "./MotionDiv";
 
 export interface AnimeProp {
   id: string;
@@ -16,13 +17,28 @@ interface Prop {
   anime: AnimeProp;
   index: number;
 }
+const variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
-function AnimeCard({ anime }: Prop) {
+function AnimeCard({ anime, index }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      transition={{ delay: index * 0.25, ease: "easeInOut", duration: 0.5 }}
+      viewport={{ amount: 0 }}
+      className="max-w-sm rounded relative w-full "
+    >
       <div className="relative w-full h-[37vh]">
         <Image
-          src={anime.image.original}
+          src={`https://shikimori.one${anime.image.original}`}
           alt={anime.name}
           fill
           className="rounded-xl"
@@ -45,7 +61,7 @@ function AnimeCard({ anime }: Prop) {
               src="./episodes.svg"
               alt="episodes"
               width={20}
-              height={20}
+              height={25}
               className="object-contain"
             />
             <p className="text-base text-white font-bold">
@@ -64,7 +80,7 @@ function AnimeCard({ anime }: Prop) {
           </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,9 +11,12 @@ function Hero() {
           height={96}
           className="object-contain"
         />
-        <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
-          Explore The <span className="red-gradient">Diverse Realms</span> of
-          Anime Magic
+        <h1 className="text-2xl  sm:text-6xl  text-white lg:max-w-lg font-bold leading-[120%]">
+          Explore The{" "}
+          <span className="text-3xl sm:text-6xl red-gradient">
+            Diverse Realms
+          </span>{" "}
+          of Anime Magic
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
